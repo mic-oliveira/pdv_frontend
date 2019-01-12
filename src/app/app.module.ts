8701909378e3cbae03c6ceb2cc775f66643bfae9
Michael <oliveiramichael@outlook.com>
2019-01-12 16:06:02 -0200

    Login - Validação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {ClarityModule, ClrAlert, ClrAlertModule} from '@clr/angular';
+import {ClarityModule, ClrAlert, ClrAlertModule, ClrIfOpen, ClrTooltipModule} from '@clr/angular';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxMaskModule} from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,13 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ClarityModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {User} from '../modelo/user';
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {invalid} from '../../../node_modules/@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   invalid = false;
   user: User = new User();
-
+  @ViewChild('login') login;
+  public padraoCustom = {'A': { pattern: new RegExp('\[^\\s,+=\]')}};
   constructor() { }
 
   ngOnInit() {
   }
-  
-  clickLogin(){
-    
+  clickLogin() {
+    if (this.login.invalid) {
+      this.invalid = true;
+    }
   }
 
 }
