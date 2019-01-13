b2ea9887eed9b47b9db422946672747e02079864
Michael <oliveiramichael@outlook.com>
2019-01-13 20:26:40 -0200

    Menu, telas produto e categoria

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Categoria} from '../modelo/categoria';
+import {ClrDatagridSortOrder} from '@clr/angular';
 
 @Component({
   selector: 'app-categoria',
@@ -7,10 +8,16 @@ import {Categoria} from '../modelo/categoria';
   styleUrls: ['./categoria.component.css']
 })
 export class CategoriaComponent implements OnInit {
+  load = false;
+  page = 5;
+  ordenacao = ClrDatagridSortOrder.ASC;
   categorias: Categoria[] = [];
   constructor() { }
 
   ngOnInit() {
   }
-
+  getProdutos() {}
+  clickAdicionar() {}
+  clickEditar() {}
+  clickDeletar() {}
 }
--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Produto} from '../modelo/produto';
+import {ClrDatagridSortOrder} from '@clr/angular';
 
 @Component({
   selector: 'app-produto',
@@ -7,10 +8,18 @@ import {Produto} from '../modelo/produto';
   styleUrls: ['./produto.component.css']
 })
 export class ProdutoComponent implements OnInit {
+  load = false;
+  page = 5;
+  ordenacao = ClrDatagridSortOrder.ASC;
   produtos: Produto[] = [];
   constructor() { }
 
   ngOnInit() {
   }
+
+  getProdutos() {}
+  clickAdicionar() {}
+  clickEditar() {}
+  clickDeletar() {}
 
 }
