0c128e9e0f6a2d5be82b7468dfb14902a9264784
Michael <oliveiramichael@outlook.com>
2019-01-13 19:26:36 -0200

    Menu, telas produto e categoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {PrincipalComponent} from './principal/principal.component';
 import {ProdutoComponent} from './produto/produto.component';
+import {CategoriaComponent} from './categoria/categoria.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'dashboard', component: PrincipalComponent, children: [
       {path: 'produtos', component: ProdutoComponent},
+      {path: 'categorias', component: CategoriaComponent},
     ]},
 ];
 
