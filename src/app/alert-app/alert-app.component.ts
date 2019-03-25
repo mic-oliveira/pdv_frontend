0b6eee16c7cef6c8521b3ba489f2f7198d06280a
Michael <oliveiramichael@outlook.com>
2019-03-24 22:46:00 -0300

    F-001---alert-app-level Criacao de componente para chamar alert através de serviço.

--- src/app/alert-app/alert-app.component.ts
+++ src/app/alert-app/alert-app.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertApp} from '../modelo/alert-app';
+import {AlertService} from '../services/alert.service';
 
 @Component({
-  selector: 'app-alert-app',
+  selector: 'app-alert',
   templateUrl: './alert-app.component.html',
   styleUrls: ['./alert-app.component.css']
 })
 export class AlertAppComponent implements OnInit {
-
-  constructor() { }
+  show = false;
+  alert: AlertApp = new AlertApp();
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.alertService.alertaApp.subscribe( (x: AlertApp) => {
+      this.alert = x;
+      this.show = x.visible;
+      console.log(x);
+    });
   }
-
 }
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertService} from '../services/alert.service';
+import {AlertApp} from '../modelo/alert-app';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css'],
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
   }
-
 }
